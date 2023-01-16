5743e065f8a5b11b4f8d18aff55ce7a021f93185
Ilia <sinevi@gmail.com>
2023-01-16 13:59:10 +0100

    new setup file for lab 1

---
+++ Allfiles/01/setup2.ps1
@@ -0,0 +1,36 @@
+Clear-Host
+write-host "Starting script at $(Get-Date)"
+
+$resourceGroupName="Spielwiese_Ilia_Sinev"
+$dataLakeAccountName="dl-spielwiese"
+
+# Upload files
+write-host "Loading data..."
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $dataLakeAccountName
+$storageContext = $storageAccount.Context
+Get-ChildItem "./data/*.csv" -File | Foreach-Object {
+    write-host ""
+    $file = $_.Name
+    Write-Host $file
+    $blobPath = "sales/csv/$file"
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+Get-ChildItem "./data/*.parquet" -File | Foreach-Object {
+    write-host ""
+    Write-Host $_.Name
+    $folder = $_.Name.Replace(".snappy.parquet", "")
+    $file = $_.Name.Replace($folder, "orders")
+    $blobPath = "sales/parquet/year=$folder/$file"
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+Get-ChildItem "./data/*.json" -File | Foreach-Object {
+    write-host ""
+    $file = $_.Name
+    Write-Host $file
+    $blobPath = "sales/json/$file"
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+write-host "Script completed at $(Get-Date)"
